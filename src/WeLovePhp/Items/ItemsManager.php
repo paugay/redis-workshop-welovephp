18d052885b3bdb6d61094f080ce4c6675e0a2147
Pau Gay <pau.gay@gmail.com>
2013-04-24 10:38:56 +0200

    Resolved Task 1.3

--- src/WeLovePhp/Items/ItemsManager.php
+++ src/WeLovePhp/Items/ItemsManager.php
@@ -32,9 +32,9 @@ class ItemsManager
             ':title' => $title,
         ));
 
-        $id = $this->connection->lastInsertId();
+        $item = $this->load($this->connection->lastInsertId());
 
-        $this->redis->lpush("items", $id);
+        $this->redis->lpush("items", json_encode($item));
         $this->redis->ltrim("items", 0, 10);
 
         return $id;
@@ -44,24 +44,20 @@ class ItemsManager
     {
         $item = $this->loadByTitle($title);
 
-        $this->redis->lrem("items", 0, $item->id);
+        $this->redis->lrem("items", 0, json_encode($item));
 
         $this->connection->delete('items', array('id' => $item->id));
     }
 
-    public function getItems($ids)
-    {
-        $items = $this->connection->executeQuery('SELECT * FROM items WHERE id IN (?)',
-            array($ids),
-            array(\Doctrine\DBAL\Connection::PARAM_INT_ARRAY)
-        )->fetchAll();
-
-        return array_reverse($items);
-    }
-
     public function getLatestItems($n)
     {
-        $ids =  $this->redis->lrange("items", 0, $n - 1);
-        return $this->getItems($ids);
+        $items =  $this->redis->lrange("items", 0, $n - 1);
+
+        return array_map(
+            function ($item) {
+                return json_decode($item, TRUE);
+            },
+            $items
+        );
     }
 }
